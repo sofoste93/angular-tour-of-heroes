d7a18264c891adab42a593f323964838a624bcc5
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2023-08-08 15:04:29 +0200

    Service added: message, heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,30 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  
-  heroes: Hero[] = [];
+export class HeroesComponent implements OnInit {
+
   selectedHero?: Hero;
-  
-  onSelect(hero: Hero): void { // new method
-    this.selectedHero = hero;
-  }
 
-  constructor(private heroService: HeroService) { }
+  heroes: Hero[] = [];
 
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-    //this.heroes = this.heroService.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -6,11 +6,11 @@ export const HEROES: Hero[] = [
     { id: 12, name: 'SuperPatrick' },
     { id: 13, name: 'SuperThomass' },
     { id: 14, name: 'SuperMichaels' },
-    { id: 15, name: 'SuperAlexs' },
+    { id: 15, name: 'SuperValeria' },
     { id: 16, name: 'SuperDeniss' },
     { id: 18, name: 'SuperAzubis' },
     { id: 19, name: 'SuperStudents' },
-    { id: 20, name: 'SuperDocu'},
+    { id: 20, name: 'SuperAlexs'},
     { id: 21, name: 'SuperHRs'},
     { id: 22, name:'superAdmins'}
 ];
