f0c009da74335f83e2747f9c19e36b4adf4175c5
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2023-08-08 10:46:13 +0200

    Tour of heroes angular project: Sofoste ALT.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+  
+  onSelect(hero: Hero): void { // new method
+    this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+    //this.heroes = this.heroService.getHeroes();
+  }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 10, name: 'SuperMario' },
+    { id: 11, name: 'SuperRamona' },
+    { id: 12, name: 'SuperPatrick' },
+    { id: 13, name: 'SuperThomass' },
+    { id: 14, name: 'SuperMichaels' },
+    { id: 15, name: 'SuperAlexs' },
+    { id: 16, name: 'SuperDeniss' },
+    { id: 18, name: 'SuperAzubis' },
+    { id: 19, name: 'SuperStudents' },
+    { id: 20, name: 'SuperDocu'},
+    { id: 21, name: 'SuperHRs'},
+    { id: 22, name:'superAdmins'}
+];
